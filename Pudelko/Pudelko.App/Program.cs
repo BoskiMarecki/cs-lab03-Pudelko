0c8291d8f510c0960b8aed0945e424a498772073
BoskiMarecki <72198070+BoskiMarecki@users.noreply.github.com>
2020-11-25 14:22:15 +0100

    Add project files.

---
+++ Pudelko/Pudelko.App/Program.cs
@@ -0,0 +1,38 @@
+﻿using PudelkoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PudelkoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Pudełko toString: ");
+            Console.WriteLine(new Pudelko(2.5, 9.321, 0.1, UnitOfMeasure.meter).ToString);
+            Console.WriteLine(new Pudelko(2.5, 9.321, 0.1, UnitOfMeasure.meter).ToString("m"));
+            Console.WriteLine(new Pudelko(2.5, 9.321, 0.1, UnitOfMeasure.meter).ToString("cm"));
+            Console.WriteLine(new Pudelko(2.5, 9.321, 0.1, UnitOfMeasure.meter).ToString("mm"));
+
+            Console.WriteLine("\n Objetosc: ");
+            Console.WriteLine(new Pudelko(1, 1, 1, UnitOfMeasure.meter).ToString);
+            Console.WriteLine(new Pudelko(1, 1, 1, UnitOfMeasure.meter).ToString("m"));
+            Console.WriteLine(new Pudelko(1, 1, 1, UnitOfMeasure.meter).ToString("cm"));
+
+            Console.WriteLine("\n Pole: ");
+            Console.WriteLine(new Pudelko(2.5, 9.321, 0.1, UnitOfMeasure.meter).ToString);
+            Console.WriteLine(new Pudelko(2.5, 9.321, 0.1, UnitOfMeasure.meter).ToString("m"));
+            Console.WriteLine(new Pudelko(2.5, 9.321, 0.1, UnitOfMeasure.meter).ToString("cm"));
+            Console.WriteLine(new Pudelko(2.5, 9.321, 0.1, UnitOfMeasure.meter).ToString("mm"));
+
+        }
+
+        /*private static int ComparePudelka(Pudelko p1, Pudelko p2)
+        {
+            
+        }*/
+
+
+    }
+}
